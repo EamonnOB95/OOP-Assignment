9ecc601bdf52948c9994fda0e7ba130ee4550e3c
EamonnOB95 <80485627+EamonnOB95@users.noreply.github.com>
2021-03-25 09:27:08 +0000

    Update Cone.java

--- Cone.java
+++ Cone.java
@@ -1,41 +1,40 @@
-public class Cone extends circleBase {
-  // Class data
-  private int height;
+public class Cone extends CircleBase {
+    // Class data
+    private int height;
 
-  // Constructors
-  public Cone() {
-    super();
-    this.height = 1;
-  }
-  public Cone(int aRadius, int aHeight) {
-    super(aRadius);
-    this.height = aHeight;
-  }
+    // Contructors
+    public Cone() {
+        super();
+        setHeight(1);
+    }
 
-  // Public methods
-  public double calculateVolume() {
-    // Formula for cone volume 
-    // πr^2h/3
-    double r=this.radius*1.0, h=this.height*1.0;
-    return Math.PI*Math.pow(r,2)*(h/3);
-  }
+    public Cone(int radius, int height) {
+        super(radius);
+        setHeight(height);
+    }
 
-  public double calculateSurfaceArea() {
-    // Forumla for cone surface area
-    // πr(r+sqrt(h^2+r^2))
-    double r=this.radius*1.0, h=this.height*1.0;
-    return Math.PI*r*(r+Math.sqrt(Math.pow(h,2)+Math.pow(r,2)));
-  }
+    // Public methods
+    @Override
+    public double surfaceArea() {
+        return (Math.PI * getRadius()) * (getRadius() + Math.sqrt(Math.pow(getHeight(), 2) + Math.pow(getRadius() ,2)));
+    }
 
-  public String getType() {
-    return "Cone";
-  }
+    public double volume() {
+        return (1/3d) * (Math.PI * Math.pow(this.getRadius(), 2) * this.getHeight());
+    }
 
-  // Getters and setters
-  public void setHeight(int aHeight) {
-    this.height = aHeight;
-  }
-  public int getHeight() {
-    return this.height;
-  }
+    @Override
+    public String getType() {
+        return "Cone";
+    }
+
+    // Getters and setters
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
+
